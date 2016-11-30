f7a7a6f1509bc6fc4e3848135c45512e469fb4a5
Adam Furbee <adam.furbee@gmail.com>
2016-11-30 17:05:42 -0600

    initial clone from SlothTorrent repo

---
+++ tests/context.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+thisdir = os.path.dirname(__file__)
+sys.path.insert(0, os.path.abspath(os.path.join(thisdir, '..')))
+
+import bencode
+# import client
+import message
+# import session
+# import torrent
+# import tracker
+# import util
